6a471233c102fa5b33fb3059c52e03e0b8743ed6
Marcos <marcos.renan@gmail.com>
2011-03-11 13:57:00 -0300

    Added: build cloud map (hash wiht post_usage=>tag_list)

--- app/models/blog_tag.rb
+++ app/models/blog_tag.rb
@@ -12,4 +12,15 @@ class BlogTag < ActiveRecord::Base
     posts.select(&:live?).count
   end
 
+  def self.build_cloud_map
+    all_tags = self.all
+    the_map = {}
+    all_tags.each do |t|
+      if the_map[t.post_count].nil?
+        the_map[t.post_count] = []
+      end
+      the_map[t.post_count] << t
+    end
+    the_map
+  end
 end
